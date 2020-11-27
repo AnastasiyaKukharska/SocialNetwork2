8b58192327c5b945686eb7c0e3502d46c7eb39bf
AnastasiyaKukharska <70960052+AnastasiyaKukharska@users.noreply.github.com>
2020-11-27 23:41:41 +0200

    Add files via upload

---
+++ DAL.MongoDB/Interfaces/IPostDal.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.MongoDB.Interfaces
+{
+    public interface IPostDal
+    {
+          void ReadAll();
+        Task CreatePostAsync(string e);
+        Task PostOverviewAsync(string n, string s, string e);
+
+    }
+}
